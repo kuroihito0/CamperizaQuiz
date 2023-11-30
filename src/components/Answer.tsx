292aba292f97a636213650710071cf255275df89
kuroihito0 <hikaru9g@gmail.com>
2023-11-30 10:11:25 +0900

    DBから値を取り出し、問題文で表示

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/answer.css';
+import AnimatedComponent from './AnimatedComponent';
 
-const Answer = ({ handleAnswerButtonClick, questions, currentQuestion }) => {
+// Fisher-Yatesアルゴリズムを使用して、配列をランダムにシャッフルする関数
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+const Answer = ({ handleAnswerButtonClick, questions, currentQuestion, getModaniData }) => {
+    const [showResults, setShowResults] = useState(false);
+
+    useEffect(() => {
+        // コンポーネントがマウントされたときに1回だけ実行
+        getMondai();
+    }, []); // 空の依存配列を渡すことで初回のみ実行されます
+
+    const getMondai = async () => {
+        await getModaniData();
+        setShowResults(true);
+    };
+
+    // ランダムに質問をシャッフル
+    const shuffledQuestions = shuffleArray(questions);
+
     return (
         <div className='body'>
             <div className="title_box">
-    <div className="title_box-body">
-        <h1 className='title'>第{currentQuestion + 1}問</h1>
-    </div>
-</div>
-            xx
-            <ul className=''>
-                {
-                    questions[currentQuestion].answerOptions.map((answerOption, key) => (
-                        <li className='question2 box2'
-                            key={key}//key、12行目にkey={key}がありますがReactではliタグを付けるときは他と被らない番号をつけないといけないためにあります。
-                            onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}
-                        </li>)
-                    )
-                }
-            </ul>
+            </div>
+            {showResults ? (
+                <div>
+                    {/* 結果を表示するコンポーネントや要素 */}
+                    <p>Q.{currentQuestion + 1}</p>
+                    <p>{shuffledQuestions[currentQuestion].questionText}</p>
+                    <ul>
+                        {shuffledQuestions[currentQuestion].answerOptions.map((answerOption, key) => (
+                            <li
+                                key={key}
+                                onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}
+                            >
+                                {answerOption.answerText}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
+                <p>Loading...</p>
+            )}
         </div>
     );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
 import Quiz from "./Quiz";
+import {motion} from "framer-motion"
+
 function Home() {
+    const [isVisible,setIsVisible] = useState(false);
+    useEffect(()=>{
+        setIsVisible(false); // ページがロードされたらisVisibleをtrueに設定してアニメーションをトリガー
+        const timer = setTimeout(()=>{
+            setIsVisible(true);
+        },2000);//秒後にisVisibleをfalseに設定（アニメーションの時間に合わせて調整してください）
+
+        return () => clearTimeout(timer);// コンポーネントがアンマウントされたらタイマーをクリア
+    },[]);
 
     return (
-        <div className="App">
-            <ul>
-                <li><Link to="/">ホーム</Link></li>
-                <li><Link to="Quiz">クイズ</Link></li>
-            </ul>
-    </div>
+        <div>
+            {isVisible &&(
+            <motion.div
+                initial={{opacity:0}}
+                animate={{opacity:1}}
+                transition={{duration:1}}>
+                <div><Link to="/">ホーム</Link></div>
+                <div><Link to="Quiz">クイズ</Link></div>
+            </motion.div>
+            )}
+        </div>
     );
     }
 
